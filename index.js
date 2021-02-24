ed4193dd7990df64e6309cdc6227e5d345f639c6
David Zimmerman <david.zimmerman@ibm.com@macbook-pro-46.home>
2021-02-24 15:01:34 -0800

    Reorganized and simplified the code.

--- index.js
+++ index.js
@@ -1,190 +1,130 @@
-/**
- * This action will read from Cloud Object Storage.  If the Cloud Object Storage
- * service is not bound to this action or to the package containing this action,
- * then you must provide the service information as argument input to this function.
- * Cloud Functions actions accept a single parameter, which must be a JSON object.
- *
- * In this case, the args variable will look like:
- *   {
- *     "bucket": "your COS bucket name",
- *     "key": "Name of the object to read"
- *   }
- */
 
-const CloudObjectStorage = require('ibm-cos-sdk');
+// Cloud Obect Storage authentication
+const COS = require('ibm-cos-sdk');
+const clientInfo = {
+  iam_apikey: 'a_aIYyQRDrym0vjS0wbWC-mYOLHPQWv4SnZx3JEddAfT',
+};
+const client = new COS.ResourceConfigurationV1(clientInfo);
+const csv=require('csvtojson');
 
-async function main(args) {
-  var cos = await new CloudObjectStorage.S3(cosConfig);
-  let doc = await getCOS();
-  let processedRows = await processRows(doc);
-  let translatedRows = await translateLanguage(processedRows);
-  let result = await pushToDiscovery(translatedRows);
-  console.log("Result: " + result);
-  return {
-    message: result
-  };
-}
+// Translator authentication
+const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
-function main(){} // call translate, call discovery, move instances to the top
+const languageTranslator = new LanguageTranslatorV3({
+  version: '2018-05-01',
+  authenticator: new IamAuthenticator({
+    apikey: 'iwtYcJp1lyY_ZCscoTEc5FkkfF2cHblTs22q-ZcnGwOg',
+  }),
+  serviceUrl: 'https://api.us-east.language-translator.watson.cloud.ibm.com/instances/27aa3a71-8664-4647-9e10-a25679b2bf87',
+});
 
- async function main(args) {
-  const { cos, params } = getParamsCOS(args, CloudObjectStorage);
-  let response;
-  const result = params;
+// Discovery authentication
+const DiscoveryV2 = require('ibm-watson/discovery/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
-  if (!params.bucket || !params.key || !cos) {
-    result.message = "bucket name, key, and apikey are required for this operation.";
-    throw result;
-  }
+const discovery = new DiscoveryV2({
+  version: '2019-11-22',
+  authenticator: new IamAuthenticator({
+    apikey: 'twO_ZJ-vMgIjlEBpJ2FDZhBu3slARh51V78jBGEFzfua',
+  }),
+  serviceUrl: 'https://api.us-east.discovery.watson.cloud.ibm.com/instances/5f5192ff-6d72-4e06-a8d0-2ff969dbda05',
+});
 
-  try {
-    response = await cos.getObject({ Bucket: params.bucket, Key: params.key }).promise();
-  } catch (err) {
-    console.log(err);
-    result.message = err.message;
-    throw result;
-  }
-  result.body = response.Body.toString();
-  return result;
-}
+// Main function
+async function main(params) {
 
-function getParamsCOS(args, COS) {
-  var bxCredsApiKey;
-  var bxCredsResourceInstanceId;
-
-  if (args.__bx_creds && args.__bx_creds['cloud-object-storage']) {
-    if (args.__bx_creds['cloud-object-storage'].apikey) {
-      bxCredsApiKey = args.__bx_creds['cloud-object-storage'].apikey;
-    }
-    if (args.__bx_creds['cloud-object-storage'].resource_instance_id) {
-      bxCredsResourceInstanceId = args.__bx_creds['cloud-object-storage'].resource_instance_id;
-    }
-  }
-
-  const { bucket, key } = args;
-  const endpoint = args.endpoint || 's3.us.cloud-object-storage.appdomain.cloud';
-  const ibmAuthEndpoint = args.ibmAuthEndpoint || 'https://iam.cloud.ibm.com/identity/token';
-  const apiKeyId = args.apikey || args.apiKeyId || bxCredsApiKey || process.env.__OW_IAM_NAMESPACE_API_KEY;
-  const serviceInstanceId = args.resource_instance_id || args.serviceInstanceId || bxCredsResourceInstanceId;
-
-  const params = {};
-  params.bucket = bucket;
-  params.key = key;
-
-  if (!apiKeyId) {
-    const cos = null;
-    return { cos, params};
-  }
-
-  const cos = new COS.S3({
-    endpoint, ibmAuthEndpoint, apiKeyId, serviceInstanceId,
-  });
-  return { cos, params };
-}
-
-/**
-csvStr:
-1,2,3
-4,5,6
-7,8,9
-*/
-const csv=require('csvtojson')
-csv({
-	noheader:true,
-	output: "csv"
-})
-csvStr = result.body;
-.fromString(csvStr)
-.then((csvRow)=>{ 
-	console.log(csvRow) // => [["1","2","3"], ["4","5","6"], ["7","8","9"]]
-})
-
-const LanguageTranslatorV3 = require('watson-developer-cloud/language-translator/v3');
-const extend = require('extend');
-const vcap = require('vcap_services');
-
-function main(){} // call translate, call discovery
-
-for (let i = 1; i < csvRow.length; i++) { // i=1 to ignore headers
-
-  //translate
+  let processedRows = await readObject(params);
+  for (let i = 1; i < processedRows.length; i++) {} // i=1 to ignore headers
+    text = processedRows[i][8]; // Index for the "text" cell
+    let translatedText = await translateRow(text);
+    let result = await discoveryAddDoc(translatedText, params);
+    console.log("Result: " + result);
   
-  text = csvRow[i][8]; // 8 for text column
-
-  function main(text, params) {
-    return new Promise((resolve, reject) => {
-      const _params = vcap.getCredentialsFromServiceBind(
-        params,
-        'language-translator',
-        'language_translator'
-      );
-      _params.headers = extend(
-        {},
-        _params.headers,
-        { 'User-Agent': 'openwhisk' }
-      );
-      let service;
-      
-      _params.push({text:text, target:"en"});
-      
-      try {
-        service = new LanguageTranslatorV3(_params);
-        service.translate(_params, (err, response) => {
-          if (err) {
-            reject(err.message);
-          } else {
-            resolve(response);
-          }
-        });
-      } catch (err) {
-        reject(err.message);
-        return;
-      }
-    });
-  }
-
-  global.main = main;
-  module.exports.test = main;
-
-  const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
-  const extend = require('extend');
-  const vcap = require('vcap_services');
-
-  //add-document
-  function main(text, params) {
-    return new Promise((resolve, reject) => {
-      const _params = vcap.getCredentialsFromServiceBind(params, 'discovery');
-      _params.headers = extend(
-        {},
-        _params.headers,
-        { 'User-Agent': 'openwhisk' }
-      );
-      const fileParams = ['file'];
-      fileParams.filter(fileParam => _params[fileParam]).forEach((fileParam) => {
-        try {
-          _params[fileParam] = Buffer.from(_params[fileParam], 'base64');
-        } catch (err) {
-          reject(err.message);
-          return;
-        }
-      });
-      let service;
-      try {
-        service = new DiscoveryV1(_params);
-        service.addDocument(text, (err, response) => { // add Document if just text
-          if (err) {
-            reject(err.message);
-          } else {
-            resolve(response);
-          }
-        });
-      } catch (err) {
-        reject(err.message);
-        return;
-      }
-    });
-  }
-
-  global.main = main;
-  module.exports.test = main;
+  return {message: result};
 }
+
+// readObject: read COS document and process rows
+async function readObject(params) {
+
+  const readObjectParams = {
+    Bucket: params.bucket,
+    Key: params.key
+  }
+
+  let COSObject = COS.getObject(readObjectParams)
+  csvStr = COSObject.Body.toString();
+
+  // Process from CSV String to CSV Row
+  csvStr = result.body;
+
+  csv({
+    noheader:true,
+    output: "csv"
+  })
+  .fromString(csvStr)
+  .then((csvRow)=>{ 
+    console.log(csvRow) // => [["1","2","3"], ["4","5","6"], ["7","8","9"]]
+  })
+
+  return csvRow
+}
+
+// translateRow: translate the text
+async function translateRow(text) { // Does the language have to be identified?
+
+  /*
+  // Identify language
+  const translateParams = {
+    text: text
+  };
+
+  languageTranslator.identify(translateParams)
+  .then(identifiedLanguages => {
+    console.log(JSON.stringify(identifiedLanguages, null, 2));
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+  */ 
+
+  const translateParams = {
+    text: text,
+    target: 'en',
+  };
+
+  languageTranslator.translate(translateParams)
+  .then(translationResult => {
+    console.log(JSON.stringify(translationResult, null, 2));
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+
+  return translationResult
+}
+
+// discoveryAddDoc: add Document to Discovery
+async function discoveryAddDoc(text, params) {
+
+  const addDocParams = {
+    projectId: 'ba9239ad-c0a3-4007-a2f5-e042acdf0a9c', // Is project ID the same as Environment ID?
+    collectionId: 'c09a011f-3abb-426a-9cba-d8ab755f86ea',
+    file: text,
+    filename: params.key,
+    fileContentType: params.notification.content_type
+    // Do you want any metadata?
+  };
+
+  discovery.addDocument(addDocParams)
+    .then(response => {
+      console.log(JSON.stringify(response.result, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+
+  return response
+}
+
+exports.main = main;
