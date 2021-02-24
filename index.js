6ed84bcec8809e1daae9c1a5fe27b65b26b5b636
David Zimmerman <david.zimmerman@ibm.com@macbook-pro-46.home>
2021-02-24 08:37:49 -0800

    Added translator and Discovery scripts.

--- index.js
+++ index.js
@@ -11,25 +11,34 @@
  *   }
  */
 
-const CloudObjectStorage = require("ibm-cos-sdk");
+const CloudObjectStorage = require('ibm-cos-sdk');
 
 async function main(args) {
-  console.log("main function was.");
+  var cos = await new CloudObjectStorage.S3(cosConfig);
+  let doc = await getCOS();
+  let processedRows = await processRows(doc);
+  let translatedRows = await translateLanguage(processedRows);
+  let result = await pushToDiscovery(translatedRows);
+  console.log("Result: " + result);
+  return {
+    message: result
+  };
+}
 
+function main(){} // call translate, call discovery, move instances to the top
+
+ async function main(args) {
   const { cos, params } = getParamsCOS(args, CloudObjectStorage);
   let response;
   const result = params;
 
   if (!params.bucket || !params.key || !cos) {
-    result.message =
-      "bucket name, key, and apikey are required for this operation.";
+    result.message = "bucket name, key, and apikey are required for this operation.";
     throw result;
   }
 
   try {
-    response = await cos
-      .getObject({ Bucket: params.bucket, Key: params.key })
-      .promise();
+    response = await cos.getObject({ Bucket: params.bucket, Key: params.key }).promise();
   } catch (err) {
     console.log(err);
     result.message = err.message;
@@ -43,30 +52,20 @@ function getParamsCOS(args, COS) {
   var bxCredsApiKey;
   var bxCredsResourceInstanceId;
 
-  if (args.__bx_creds && args.__bx_creds["cloud-object-storage"]) {
-    if (args.__bx_creds["cloud-object-storage"].apikey) {
-      bxCredsApiKey = args.__bx_creds["cloud-object-storage"].apikey;
+  if (args.__bx_creds && args.__bx_creds['cloud-object-storage']) {
+    if (args.__bx_creds['cloud-object-storage'].apikey) {
+      bxCredsApiKey = args.__bx_creds['cloud-object-storage'].apikey;
     }
-    if (args.__bx_creds["cloud-object-storage"].resource_instance_id) {
-      bxCredsResourceInstanceId =
-        args.__bx_creds["cloud-object-storage"].resource_instance_id;
+    if (args.__bx_creds['cloud-object-storage'].resource_instance_id) {
+      bxCredsResourceInstanceId = args.__bx_creds['cloud-object-storage'].resource_instance_id;
     }
   }
 
   const { bucket, key } = args;
-  const endpoint =
-    args.endpoint || "s3.us.cloud-object-storage.appdomain.cloud";
-  const ibmAuthEndpoint =
-    args.ibmAuthEndpoint || "https://iam.cloud.ibm.com/identity/token";
-  const apiKeyId =
-    args.apikey ||
-    args.apiKeyId ||
-    bxCredsApiKey ||
-    process.env.__OW_IAM_NAMESPACE_API_KEY;
-  const serviceInstanceId =
-    args.resource_instance_id ||
-    args.serviceInstanceId ||
-    bxCredsResourceInstanceId;
+  const endpoint = args.endpoint || 's3.us.cloud-object-storage.appdomain.cloud';
+  const ibmAuthEndpoint = args.ibmAuthEndpoint || 'https://iam.cloud.ibm.com/identity/token';
+  const apiKeyId = args.apikey || args.apiKeyId || bxCredsApiKey || process.env.__OW_IAM_NAMESPACE_API_KEY;
+  const serviceInstanceId = args.resource_instance_id || args.serviceInstanceId || bxCredsResourceInstanceId;
 
   const params = {};
   params.bucket = bucket;
@@ -74,14 +73,11 @@ function getParamsCOS(args, COS) {
 
   if (!apiKeyId) {
     const cos = null;
-    return { cos, params };
+    return { cos, params};
   }
 
   const cos = new COS.S3({
-    endpoint,
-    ibmAuthEndpoint,
-    apiKeyId,
-    serviceInstanceId
+    endpoint, ibmAuthEndpoint, apiKeyId, serviceInstanceId,
   });
   return { cos, params };
 }
@@ -92,11 +88,103 @@ csvStr:
 4,5,6
 7,8,9
 */
-const csv = require("csvtojson");
+const csv=require('csvtojson')
 csv({
-  noheader: true,
-  output: "csv"
-});
-csvStr = result.body.fromString(csvStr).then(csvRow => {
-  console.log(csvRow); // => [["1","2","3"], ["4","5","6"], ["7","8","9"]]
-});
+	noheader:true,
+	output: "csv"
+})
+csvStr = result.body;
+.fromString(csvStr)
+.then((csvRow)=>{ 
+	console.log(csvRow) // => [["1","2","3"], ["4","5","6"], ["7","8","9"]]
+})
+
+const LanguageTranslatorV3 = require('watson-developer-cloud/language-translator/v3');
+const extend = require('extend');
+const vcap = require('vcap_services');
+
+function main(){} // call translate, call discovery
+
+for (let i = 1; i < csvRow.length; i++) { // i=1 to ignore headers
+
+  //translate
+  
+  text = csvRow[i][8]; // 8 for text column
+
+  function main(text, params) {
+    return new Promise((resolve, reject) => {
+      const _params = vcap.getCredentialsFromServiceBind(
+        params,
+        'language-translator',
+        'language_translator'
+      );
+      _params.headers = extend(
+        {},
+        _params.headers,
+        { 'User-Agent': 'openwhisk' }
+      );
+      let service;
+      
+      _params.push({text:text, target:"en"});
+      
+      try {
+        service = new LanguageTranslatorV3(_params);
+        service.translate(_params, (err, response) => {
+          if (err) {
+            reject(err.message);
+          } else {
+            resolve(response);
+          }
+        });
+      } catch (err) {
+        reject(err.message);
+        return;
+      }
+    });
+  }
+
+  global.main = main;
+  module.exports.test = main;
+
+  const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+  const extend = require('extend');
+  const vcap = require('vcap_services');
+
+  //add-document
+  function main(text, params) {
+    return new Promise((resolve, reject) => {
+      const _params = vcap.getCredentialsFromServiceBind(params, 'discovery');
+      _params.headers = extend(
+        {},
+        _params.headers,
+        { 'User-Agent': 'openwhisk' }
+      );
+      const fileParams = ['file'];
+      fileParams.filter(fileParam => _params[fileParam]).forEach((fileParam) => {
+        try {
+          _params[fileParam] = Buffer.from(_params[fileParam], 'base64');
+        } catch (err) {
+          reject(err.message);
+          return;
+        }
+      });
+      let service;
+      try {
+        service = new DiscoveryV1(_params);
+        service.addDocument(text, (err, response) => { // add Document if just text
+          if (err) {
+            reject(err.message);
+          } else {
+            resolve(response);
+          }
+        });
+      } catch (err) {
+        reject(err.message);
+        return;
+      }
+    });
+  }
+
+  global.main = main;
+  module.exports.test = main;
+}
